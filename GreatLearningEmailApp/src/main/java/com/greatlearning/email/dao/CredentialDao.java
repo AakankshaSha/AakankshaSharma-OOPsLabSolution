2f7c8948c2ece1d90828920df2fad63df7d43d78
AakankshaSha <105551501+AakankshaSha@users.noreply.github.com>
2022-06-19 21:06:31 +0530

    Delete GreatLearningEmailApp directory
    Deleting the files

--- GreatLearningEmailApp/src/main/java/com/greatlearning/email/dao/CredentialDao.java
+++
@@ -1,8 +0,0 @@
-package com.greatlearning.email.dao;
-
-import com.greatlearning.email.model.Employee;
-
-public interface CredentialDao {
-	public String generateEmailAddress(Employee emp, String department);
-	public char[] generatePassword();
-}
--- GreatLearningEmailApp/src/main/java/com/greatlearning/email/service/CredentialService.java
+++
@@ -1,9 +0,0 @@
-package com.greatlearning.email.service;
-
-import com.greatlearning.email.model.Employee;
-
-public interface CredentialService {
-	public char[] generatePassword();
-	public String generateEmailAddress(Employee emp, String department);
-	public void showCredentials(Employee emp, String department); 
-}
